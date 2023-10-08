c1b295fe93eff600b15754bfdd3a033125c65d4c
nardober1st <bernardo.oechsler6@gmail.com>
2023-10-08 00:01:48 -0700

    Onboarding, login, and signup screen added

---
+++ app/src/main/java/com/bernardooechsler/ecommerceapp/fragments/loginRegister/RegisterFragment.kt
@@ -0,0 +1,7 @@
+package com.bernardooechsler.ecommerceapp.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.bernardooechsler.ecommerceapp.R
+
+class RegisterFragment: Fragment(R.layout.fragment_register) {
+}
