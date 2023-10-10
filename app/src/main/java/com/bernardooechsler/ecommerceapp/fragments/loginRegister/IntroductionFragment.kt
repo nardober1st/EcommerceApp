d51e4c2259f84568930220869b663ca0e1eb424e
nardober1st <bernardo.oechsler6@gmail.com>
2023-10-10 02:03:01 -0700

    login and singup navigation set up

--- app/src/main/java/com/bernardooechsler/ecommerceapp/fragments/loginRegister/IntroductionFragment.kt
+++ app/src/main/java/com/bernardooechsler/ecommerceapp/fragments/loginRegister/IntroductionFragment.kt
@@ -1,7 +1,37 @@
 package com.bernardooechsler.ecommerceapp.fragments.loginRegister
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.bernardooechsler.ecommerceapp.R
+import com.bernardooechsler.ecommerceapp.databinding.FragmentIntrodcutionBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class IntroductionFragment: Fragment(R.layout.fragment_introdcution) {
+@AndroidEntryPoint
+class IntroductionFragment : Fragment(R.layout.fragment_introdcution) {
+
+    private lateinit var binding: FragmentIntrodcutionBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentIntrodcutionBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonStart.setOnClickListener {
+            findNavController().navigate(R.id.action_introductionFragment_to_accountOptionsFragment)
+        }
+    }
 }
--- app/src/main/java/com/bernardooechsler/ecommerceapp/fragments/loginRegister/RegisterFragment.kt
+++ app/src/main/java/com/bernardooechsler/ecommerceapp/fragments/loginRegister/RegisterFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.bernardooechsler.ecommerceapp.R
 import com.bernardooechsler.ecommerceapp.data.User
 import com.bernardooechsler.ecommerceapp.databinding.FragmentRegisterBinding
 import com.bernardooechsler.ecommerceapp.util.RegisterValidation
@@ -34,6 +36,10 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tvDoYouHaveAccount.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
 
         binding.apply {
             buttonRegisterRegister.setOnClickListener {
