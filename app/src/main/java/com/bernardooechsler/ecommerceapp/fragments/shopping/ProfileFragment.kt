ff1ad386551777ecbefc6e570e8843a29858fde6
nardober1st <bernardo.oechsler6@gmail.com>
2023-10-20 19:03:26 -0700

    Bottom Nav implemented, and its fragments

---
+++ app/src/main/java/com/bernardooechsler/ecommerceapp/fragments/shopping/ProfileFragment.kt
@@ -0,0 +1,22 @@
+package com.bernardooechsler.ecommerceapp.fragments.shopping
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bernardooechsler.ecommerceapp.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
