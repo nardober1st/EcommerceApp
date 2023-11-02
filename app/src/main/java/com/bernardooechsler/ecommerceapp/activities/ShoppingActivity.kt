02b3438882b2aa4a287011db3e5faf181ce5b588
nardober1st <bernardo.oechsler6@gmail.com>
2023-11-01 23:43:30 -0700

    special products adapter implemented

--- app/src/main/java/com/bernardooechsler/ecommerceapp/activities/ShoppingActivity.kt
+++ app/src/main/java/com/bernardooechsler/ecommerceapp/activities/ShoppingActivity.kt
@@ -6,7 +6,9 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bernardooechsler.ecommerceapp.R
 import com.bernardooechsler.ecommerceapp.databinding.ActivityShoppingBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ShoppingActivity : AppCompatActivity() {
 
     val binding by lazy {
